441088d795f42d8f8c38fd89030e26ac50579a3f
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-04-15 12:06:17 +0900

    Fix: 소유권 배송 요청 화면에서 기본 배송지 정보가 입력되어있지 않던 부분 수정

--- client/src/page/user/shipping/writer/address/AddressSection.tsx
+++ client/src/page/user/shipping/writer/address/AddressSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useShippingAddresses from '@api/query/shipping/useShippingAddressesQuery';
 import useModal from '@component/modal/useModal';
 import { useAppSelector } from '@app/redux/store';
-import { State, Action } from '@component/shipping/address/editor/reducer';
+import { State, Action, initialState } from '@component/shipping/address/editor/reducer';
 import AddressCard from './content/AddressCard';
 import AddressModal from './content/AddressModal';
 
@@ -14,12 +14,32 @@ interface Props {
 function AddressSection({ state, dispatch }: Props) {
   const addressModal = useModal();
   const { userId } = useAppSelector(state => state.auth);
-  const { status, data: addresses, error } = useShippingAddresses(userId);
-  
+
+  const {
+    status,
+    data: addresses,
+    error,
+  } = useShippingAddresses(userId, {
+    onSuccess: res => {
+      dispatch({
+        type: 'SET_FORM',
+        form: {
+          ...res.addresses[0],
+          name: '배송지',
+        },
+      });
+    },
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false
+  });
+
   return (
     <>
       <AddressCard modal={addressModal} state={state} dispatch={dispatch} />
-      {status === "success" && <AddressModal addresses={addresses} modal={addressModal} state={state} dispatch={dispatch} />}
+      {status === 'success' && (
+        <AddressModal addresses={addresses} modal={addressModal} state={state} dispatch={dispatch} />
+      )}
     </>
   );
 }
