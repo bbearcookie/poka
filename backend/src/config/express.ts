c01d29b14f7b11cfb1caaf2da63a766c348aff2d
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-04-15 17:53:29 +0900

    Chore: 배포와 관련된 클라이언트와 백엔드단의 URL 설정

--- backend/src/config/express.ts
+++ backend/src/config/express.ts
@@ -17,27 +17,22 @@ export const app = express();
 require('express-async-errors'); // 비동기로 발생한 오류를 express가 잡을 수 있게 하여 공통 에러 핸들러에서 처리하도록 함.
 
 // 기본 설정 함수
-export default function() {
+export default function () {
   app.use(express.json()); // express 4.16 버전부터는 body-parser가 내장되었음
   app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded 형태의 데이터 파싱 가능하게 설정
   app.use(cookieParser()); // 브라우저의 쿠키를 파싱 가능하도록 설정
-  if (process.env.INIT_CWD) app.use(express.static(path.join(process.env.INIT_CWD, "/public"))); // 정적 파일들 기본 폴더 설정
+  if (process.env.INIT_CWD) app.use(express.static(path.join(process.env.INIT_CWD, '/public'))); // 정적 파일들 기본 폴더 설정
 
-  const originList: string[] = [];
-  if (process.env.CLIENT_SERVER_URL)
-    originList.push(process.env.CLIENT_SERVER_URL);
-  if (process.env.CLIENT_SERVER_URL_WITH_WWW)
-    originList.push(process.env.CLIENT_SERVER_URL_WITH_WWW);
-    
+  const whitelist: string[] = [];
+  process.env.CLIENT_LOCALHOST_URL && whitelist.push(process.env.CLIENT_LOCALHOST_URL);
+  process.env.CLIENT_NETLIFY_URL && whitelist.push(process.env.CLIENT_NETLIFY_URL);
+
   const corsOptions = {
     origin: (origin: any, callback: any) => {
-      if (originList.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(null, false);
-      }
+      if (whitelist.indexOf(origin) !== -1) callback(null, true);
+      else callback(null, false);
     },
-    credentials: true
-  }
+    credentials: true,
+  };
   app.use(cors(corsOptions));
-};
+}
--- client/src/api/resource.ts
+++ client/src/api/resource.ts
@@ -1,4 +1,4 @@
-export const BACKEND = 'http://localhost:5000';
+export const BACKEND = process.env.REACT_APP_BACKEND_URL;
 
 export const userImage = (imageName: string | undefined) => imageName ? `${BACKEND}/image/user/${imageName}` : '/user.png';
 export const groupImage = (imageName: string | undefined) => `${BACKEND}/image/group/${imageName}`;
