bbe83efe27c37032ac4353c27334cfbdbf39f94b
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-04-15 17:02:58 +0900

    Chore: 데이터베이스 연결에 포트 번호를 .env에서 가져오도록 수정. 일부 패키지 devDependencies에 포함되도록 수정

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,13 +1,12 @@
 import mysql from 'mysql2/promise';
 
-const options = {
+const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
   connectionLimit: 30,
-};
-
-const pool = mysql.createPool(options);
+});
 
 export default pool;
