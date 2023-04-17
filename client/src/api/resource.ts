b412638dfafe729a70338c73e9e332edd698fbda
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-04-17 21:20:44 +0900

    Remove: resource.ts에서 RESOURCE_URL 값이 출력되던 부분 제거

--- client/src/api/resource.ts
+++ client/src/api/resource.ts
@@ -1,8 +1,6 @@
 export const BACKEND = process.env.REACT_APP_BACKEND_URL;
 export const RESOURCE = process.env.REACT_APP_RESOURCE_URL;
 
-console.log(RESOURCE);
-
 export const userImage = (imageName: string | undefined) =>
   imageName ? `${RESOURCE}/image/user/${imageName}` : '/user.png';
 export const groupImage = (imageName: string | undefined) => `${RESOURCE}/image/group/${imageName}`;
