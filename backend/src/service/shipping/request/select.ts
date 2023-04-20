7dd6697c2cc91804853723a70d5b6463eb2d2689
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-04-20 17:03:02 +0900

    Fix: 배송 요청 목록 조회에서 소유권 정보가 조회되지 않던 문제 수정

--- backend/src/service/shipping/request/select.ts
+++ backend/src/service/shipping/request/select.ts
@@ -7,11 +7,7 @@ import { ShippingRequestDetail, ShippingRequestItem } from '@type/shipping';
 import { FilterType } from '@controller/shipping/request/getRequests';
 
 // 배송 요청 목록 조회
-export const selectShippingRequests = async (
-  itemPerPage: number,
-  pageParam: number,
-  filter: FilterType
-) => {
+export const selectShippingRequests = async (itemPerPage: number, pageParam: number, filter: FilterType) => {
   let con: PoolConnection | undefined;
 
   try {
@@ -39,13 +35,7 @@ export const selectShippingRequests = async (
         'username', U.username,
         'nickname', U.nickname,
         'imageName', U.image_name
-      ) as author,
-      JSON_OBJECT(
-        'amount', 
-        (SELECT count(*)
-        FROM ShippingRequestVoucher as V
-        WHERE V.request_id=R.request_id)
-      ) as voucher
+      ) as author
     FROM ShippingRequest as R
     INNER JOIN Payment as P ON R.payment_id=P.payment_id
     INNER JOIN User as U ON R.user_id=U.user_id `;
@@ -54,7 +44,7 @@ export const selectShippingRequests = async (
     if (filter.userNames.length > 0) {
       where.push({
         query: `U.username IN (${con.escape(filter.userNames)})`,
-        operator: 'AND'
+        operator: 'AND',
       });
     }
 
@@ -62,7 +52,7 @@ export const selectShippingRequests = async (
     if (filter.shippingState && filter.shippingState !== 'all') {
       where.push({
         query: `R.state=${con.escape(filter.shippingState)}`,
-        operator: 'AND'
+        operator: 'AND',
       });
     }
 
@@ -70,7 +60,7 @@ export const selectShippingRequests = async (
     if (filter.paymentState && filter.paymentState !== 'all') {
       where.push({
         query: `P.state=${con.escape(filter.paymentState)}`,
-        operator: 'AND'
+        operator: 'AND',
       });
     }
 
@@ -79,36 +69,71 @@ export const selectShippingRequests = async (
     LIMIT ${con.escape(itemPerPage)} OFFSET ${con.escape(pageParam * itemPerPage)}`;
 
     // 배송 요청 목록 가져오기
-    let [requests] = await con.query<ShippingRequestItem[] & ResultSetHeader>(sql);
-    requests = parseJsonObject(requests, 'address', 'payment', 'author', 'voucher') as ShippingRequestItem[] & ResultSetHeader;
+    let [requests] = await con.query<Omit<ShippingRequestItem, 'voucher'>[] & ResultSetHeader>(sql);
+    requests = parseJsonObject(requests, 'address', 'payment', 'author') as ShippingRequestItem[] & ResultSetHeader;
 
-    // 각 배송 요청에 등록된 첫 번째 소유권의 포토카드 이미지 가져오기
-    const loadVouchers = requests.map(r => (
-      new Promise<ShippingRequestItem>(async (resolve, reject) => {
-        if (!con) throw new Error('undefined connection');
+    // 각 배송 요청마다 소유권 정보 가져오기
+    const loadVouchers = requests.map(
+      r =>
+        new Promise<ShippingRequestItem>((resolve, reject) => {
+          if (!con) throw new Error('undefined connection');
 
-        try {
-          let sql = `
-          SELECT
-            name,
-            image_name as imageName
-          FROM ShippingRequestVoucher as RV
-          INNER JOIN Voucher as V ON RV.voucher_id=V.voucher_id
-          INNER JOIN Photocard as P ON V.photocard_id=P.photocard_id
-          WHERE RV.request_id=${con.escape(r.requestId)}
-          ORDER BY V.voucher_id`;
+          // 소유권 갯수 가져오기
+          interface Amount {
+            amount: number;
+          }
+          const loadAmount: Promise<Amount> = new Promise((resolve, reject) => {
+            if (!con) throw new Error('undefined connection');
 
-          interface Voucher { name: string; imageName: string; };
-          const [[voucher]] = await con.query<Voucher[] & ResultSetHeader>(sql);
+            let sql = `
+            SELECT
+              count(*) as amount
+            FROM ShippingRequestVoucher as RV
+            WHERE RV.request_id=${con.escape(r.requestId)}`;
 
-          resolve(produce(r, draft => {
-            draft.voucher.represent = voucher;
-          }));
-        } catch (err) {
-          reject(err);
-        }
-      })
-    ));
+            con
+              .query<Amount[] & ResultSetHeader>(sql)
+              .then(result => resolve(result[0][0]))
+              .catch(reject);
+          });
+
+          // 대표 소유권 정보 가져오기
+          interface Represent {
+            name: string;
+            imageName: string;
+          }
+          const loadRepresent: Promise<Represent> = new Promise((resolve, reject) => {
+            if (!con) throw new Error('undefined connection');
+
+            let sql = `
+            SELECT
+              name,
+              image_name as imageName
+            FROM ShippingRequestVoucher as RV
+            INNER JOIN Voucher as V ON RV.voucher_id=V.voucher_id
+            INNER JOIN Photocard as P ON V.photocard_id=P.photocard_id
+            WHERE RV.request_id=${con.escape(r.requestId)}
+            ORDER BY V.voucher_id`;
+
+            con
+              .query<Represent[] & ResultSetHeader>(sql)
+              .then(result => resolve(result[0][0]))
+              .catch(reject);
+          });
+
+          Promise.all([loadAmount, loadRepresent])
+            .then(([amount, represent]) => {
+              resolve({
+                ...r,
+                voucher: {
+                  amount: amount.amount,
+                  represent,
+                },
+              });
+            })
+            .catch(reject);
+        })
+    );
 
     return await Promise.all(loadVouchers);
   } catch (err) {
@@ -116,12 +141,10 @@ export const selectShippingRequests = async (
   } finally {
     con?.release();
   }
-}
+};
 
 // 배송 요청 상세 조회
-export const selectShippingRequestDetail = async (
-  requestId: number
-) => {
+export const selectShippingRequestDetail = async (requestId: number) => {
   let con: PoolConnection | undefined;
 
   try {
@@ -166,4 +189,4 @@ export const selectShippingRequestDetail = async (
   } finally {
     con?.release();
   }
-}
+};
